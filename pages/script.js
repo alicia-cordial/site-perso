9d35a1d5963aa103233ead025d3995bae7b61199
Cordial <alicia.cordial@laplateforme.io>
2021-03-09 14:38:26 +0100

    portfolio

---
+++ pages/script.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {color()};
+
+function color() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("progress").style.width = scrolled + "%";
+}
+
