5659d435e0e1c259eb58091dead9f86294ba4ba6
Cordial <alicia.cordial@laplateforme.io>
2021-03-15 11:10:14 +0100

    menu hamburger

--- pages/portfolio.php
+++ pages/portfolio.php
@@ -4,13 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
     <title>Portfolio</title>
 </head>
 
 <body>
-<header>
-    header
+<header class="main-header">
+    <nav class="main-navigation">
+        <div class="nav-wrapper">
+
+        <input type="checkbox" id="menu-checkbox"  class="menu-checkbox" />
+        <label for="menu-checkbox" class="menu-toggle">≡ Menu</label>
+        <ul class="menu">
+    <li><a href="index.php">Accueil</a></li>
+    <li><a href="index.php">Présentation</a></li>
+    <li><a href="index.php">Contact</a></li>
+        </ul>
+
+     </div>
+   </nav>
+
 </header>
 
 <main>
