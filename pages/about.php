4a20fc065edf41ed21948cb767a130c0109822b4
Cordial <alicia.cordial@laplateforme.io>
2021-03-19 11:36:20 +0100

    about

---
+++ pages/about.php
@@ -0,0 +1,94 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+
+    <link rel= "stylesheet" href= "../style.css">
+    <title>Gluten Tag</title>
+</head>
+<body>
+<header>
+    header
+</header>
+
+<main>
+<h1>ABOUT ME</h1>
+
+    <section class="about">
+
+    <div class="parent">
+        <div class="div1">
+            <h2>ABOUT</h2>
+            <p>Je suis basée sur Marseille et actuellement en première année de formation en développement. Je prépare actuellement mon projet professionnel en partenierat avec une entreprise.</p>
+            <p>J'aime profiter de la vie, lire et voyager.</p>
+        </div>
+        <div class="div2">
+            <a href="accueil.php">
+            <h2>CONTACT</h2>
+            <p>Alicia CORDIAL</p>
+            <p>alicia.cordial@laplateforme.io</p>
+            <p>06.99.39.32.62</p>
+            </a>
+        </div>
+        <div class="div3">
+            <h2>COMPETENCES</h2>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>PHP</p>
+            <p>Javascript</p>
+
+        </div>
+        <div class="div4">
+            <a href="accueil.php">
+            <h2>FOLLOW</h2>
+            <p>GITHUB</p>
+            <p>LINKEDIN</p>
+        </a>
+        </div>
+        <div class="div5">
+            <a href="accueil.php">
+            <h2>MY WORK</h2>
+            <p>Available on my website or on Github :) </p>
+            </a>
+        </div>
+    </div>
+
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</main>
+
+
+<footer>
+    footer
+</footer>
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $('.sidenav').sidenav();
+    });
+</script>
+</body>
+</html>
