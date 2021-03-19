69f375ddebd02c05d15d3f71b597ffa14ef1d82e
Cordial <alicia.cordial@laplateforme.io>
2021-03-19 11:37:37 +0100

    liens

--- pages/about.php
+++ pages/about.php
@@ -29,7 +29,7 @@
             <p>J'aime profiter de la vie, lire et voyager.</p>
         </div>
         <div class="div2">
-            <a href="accueil.php">
+            <a href="portfolio.php">
             <h2>CONTACT</h2>
             <p>Alicia CORDIAL</p>
             <p>alicia.cordial@laplateforme.io</p>
@@ -45,14 +45,14 @@
 
         </div>
         <div class="div4">
-            <a href="accueil.php">
+            <a href="portfolio.php">
             <h2>FOLLOW</h2>
             <p>GITHUB</p>
             <p>LINKEDIN</p>
         </a>
         </div>
         <div class="div5">
-            <a href="accueil.php">
+            <a href="portfolio.php">
             <h2>MY WORK</h2>
             <p>Available on my website or on Github :) </p>
             </a>
