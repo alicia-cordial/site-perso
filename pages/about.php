518d95aa410fd5aaa212243da5591f758d969ffc
Cordial <alicia.cordial@laplateforme.io>
2021-03-19 17:56:55 +0100

    footer organisé + about remise en page

--- pages/about.php
+++ pages/about.php
@@ -16,54 +16,42 @@ session_start();
 <main>
 <h1>ABOUT ME</h1>
 
-    <section class="about">
 
-    <div class="parent">
-        <div class="div1">
+
+    <article>
+        <div class="Abdiv" id="Div1">
             <h2>ABOUT</h2>
             <p>Je suis basée sur Marseille et actuellement en première année de formation en développement. Je prépare actuellement mon projet professionnel en partenierat avec une entreprise.</p>
             <p>J'aime profiter de la vie, lire et voyager.</p>
         </div>
-        <div class="div2">
-            <a href="portfolio.php">
-            <h2>CONTACT</h2>
-            <p>Alicia CORDIAL</p>
-            <p>alicia.cordial@laplateforme.io</p>
-            <p>06.99.39.32.62</p>
-            </a>
-        </div>
-        <div class="div3">
+        <div class="Abdiv" id="Div2">
             <h2>COMPETENCES</h2>
             <p>HTML</p>
             <p>CSS</p>
             <p>PHP</p>
             <p>Javascript</p>
+        </div>
+    </article>
+    <article>
+        <div class="Abdiv" id="Div3">
+            <h2>COMPETENCES</h2>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>PHP</p>
+            <p>Javascript</p>
+        </div>
+        <div  class="Abdiv Div4">
+            <a href="projects.php">
+            <div class="text-box">
 
-        </div>
-        <div class="div4">
-            <a href="portfolio.php">
-            <h2>FOLLOW</h2>
-            <p>GITHUB</p>
-            <p>LINKEDIN</p>
-        </a>
-        </div>
-        <div class="div5">
-            <a href="portfolio.php">
-            <h2>MY WORK</h2>
-            <p>Available on my website or on Github :) </p>
+                <p>Hello,</p>
+                <p>Interested?</p>
+                <p>Come here to see my projects!</p>
+
+            </div>
             </a>
         </div>
-    </div>
-
-
-    </section>
-
-
-
-
-
-
-
+    </article>
 
 
 
