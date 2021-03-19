115d82d704cd890c3aa064a1dfc4251b318dfe6a
Cordial <alicia.cordial@laplateforme.io>
2021-03-19 16:30:58 +0100

    footer

--- pages/portfolio.php
+++ pages/portfolio.php
@@ -32,7 +32,17 @@
 
 </main>
 <footer>
-    footer
+    <section class="social">
+        <h3>Social Networks</h3>
+        <p><a href="https://www.linkedin.com/in/alicia-cordial-0538b8205/"><img src="../image/icons8-linkedin-26.png"/></a></p>
+        <p><a href="https://github.com/alicia-cordial/"><img src="../image/icons8-github-30.png"/></a></p>
+    </section>
+    <section class="infos_footer">
+        <h3>My Infos</h3>
+        <p>Alicia Cordial</p>
+        <p>School : LaPlateforme_ Marseille</p>
+        <p>alicia.cordial@laplateforme.io</p>
+    </section>
 </footer>
 
 
--- pages/projet3.php
+++ pages/projet3.php
@@ -18,6 +18,18 @@
         <li><a href="about.php"><h2>ABOUT</h2><p>Me, Myself and I</p></a></li>
     </ul>
 </header>
-
+<footer>
+    <section class="social">
+        <h3>Social Networks</h3>
+        <a href="https://www.linkedin.com/in/alicia-cordial-0538b8205/"><img src="../image/icons8-linkedin-26.png"/></a>
+        <a href="https://github.com/alicia-cordial/"><img src="../image/icons8-github-30.png"/></a>
+    </section>
+    <section class="infos_footer">
+        <h3>My Infos</h3>
+        <p>Alicia Cordial</p>
+        <p>School : LaPlateforme_ Marseille</p>
+        <p>alicia.cordial@laplateforme.io</p>
+    </section>
+</footer>
 </body>
 </html>
